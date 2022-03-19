b895c01ac807f9573e0ec43baa3e06d18726801e
Evelyn Rodriguez <evelynr812@gmail.com>
2022-03-19 12:25:37 -0400

    added data layer

---
+++ db/index.js
@@ -0,0 +1,57 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/dealers_choice_full_stack');
+
+const Diver = sequelize.define('diver', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    certificationLevel: {
+        type: Sequelize.ENUM("Open Water", "Advanced Open Water", "Rescue", "Divemaster"),
+        defaultValue: "Open Water"
+    }
+});
+
+const DiveSite = sequelize.define('diveSite', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    location: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    diverTypeAllowed: {
+        type: Sequelize.ENUM("Open Water", "Advanced Open Water", "Divemaster")
+    }
+});
+
+const syncAndSeed = async() => {
+    await sequelize.sync({ force: true });
+
+    const [ paradise, chankanaab, santaRosa, thistlegorm, daedalus, tubbataha, galera ] = await Promise.all([
+        DiveSite.create({ name: "Paradise Reef", location: "Cozumel, Mexico", diverTypeAllowed: "Open Water" }),
+        DiveSite.create({ name: "Chankanaab Reef", location: "Cozumel, Mexico", diverTypeAllowed: "Open Water" }),
+        DiveSite.create({ name: "Santa Rosa Wall", location: "Cozumel, Mexico", diverTypeAllowed: "Advanced Open Water" }),
+        DiveSite.create({ name: "U.S.S. Thistlegorm Wreck", location: "Red Sea, Egypt", diverTypeAllowed: "Open Water" }),
+        DiveSite.create({ name: "Daedalus Reef", location: "Red Sea, Egypt", diverTypeAllowed: "Advanced Open Water" }),
+        DiveSite.create({ name: "Tubbataha Reef", location: "Palawan, Philippines", diverTypeAllowed: "Open Water" }),
+        DiveSite.create({ name: "Canyons Puerto Galera", location: "Puerto Galera, Philippines", diverTypeAllowed: "Advanced Open Water" })
+    ]);
+
+    await Promise.all([
+        Diver.create({ name: "Evelyn", certificationLevel: "Open Water" }),
+        Diver.create({ name: "Jimmy", certificationLevel: "Advanced Open Water" }),
+        Diver.create({ name: "Mike", certificationLevel: "Advanced Open Water" }),
+        Diver.create({ name: "Kara", certificationLevel: "Open Water" }),
+        Diver.create({ name: "Jose", certificationLevel: "Divemaster" }),
+        Diver.create({ name: "Luisa", certificationLevel: "Divemaster" })
+    ]);
+};
+
+module.exports = {
+    sequelize,
+    Diver,
+    DiveSite,
+    syncAndSeed
+};
--- server.js
+++ server.js
@@ -1,10 +1,39 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { syncAndSeed, Diver, DiveSite } = require('./db/index');
 
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
 app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'index.html')));
 
-const port = process.env.PORT || 3000;
+app.get('/api/divers', async(req, res, next) => {
+    try {
+        res.send(await Diver.findAll());
+    }
+    catch(err) {
+        next(err);
+    }
+});
 
-app.listen(port, ()=> console.log(`listening on port ${port}`));
+app.get('/api/divesites', async(req, res, next) => {
+    try {
+        res.send(await DiveSite.findAll());
+    }
+    catch(err) {
+        next(err);
+    }
+});
+
+const setup = async() => {
+    try {
+        await syncAndSeed();
+
+        const port = process.env.PORT || 3000;
+        app.listen(port, ()=> console.log(`listening on port ${port}`));
+    }
+    catch(err) {
+        console.log(err);
+    }
+};
+
+setup();
